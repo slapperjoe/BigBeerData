58c3135aa98b2c54c6adc84bd52bcd87311768ed
slapperjoe <markr@live.com>
2022-10-24 12:28:43 +1100

    changes for azure static apps and not using blobtrigger

--- Api/FileOps/FileUpload.cs
+++ Api/FileOps/FileUpload.cs
@@ -14,71 +14,128 @@ using Microsoft.AspNetCore.WebUtilities;
 using Azure.Core;
 using Api.Helpers;
 using Microsoft.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Azure.NotificationHubs;
+using System.Xml;
 
 namespace Api.FileOps
 {
-    public class FileUpload
-    {
-        private readonly ILogger _logger;
+	public class FileUpload
+	{
+		private readonly ILogger _logger;
 
-        public FileUpload(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<FileUpload>();
-        }
+		const string MD5Key = "ParsedMD5";
 
-        [Function("FileUpload")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
+		public FileUpload(ILoggerFactory loggerFactory)
+		{
+			_logger = loggerFactory.CreateLogger<FileUpload>();
+		}
 
-            var boundary = MultipartRequestHelper.GetBoundary(
-                MediaTypeHeaderValue.Parse(req.Headers.GetValues("Content-Type").FirstOrDefault()), (int)req.Body.Length);
+		[Function("FileUpload")]
+		public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+		{
 
-            var reader = new MultipartReader(boundary, req.Body);
+			var boundary = MultipartRequestHelper.GetBoundary(
+				 MediaTypeHeaderValue.Parse(req.Headers.GetValues("Content-Type").FirstOrDefault()), (int)req.Body.Length);
 
-            var data = await reader.ReadNextSectionAsync();
+			var reader = new MultipartReader(boundary, req.Body);
 
-            while (data != null)
-            {
-                var file = data.AsFileSection();
-                using (MemoryStream ms = new())
-                {
-                    string storageConnectionString = System.Environment.GetEnvironmentVariable("BigBeerStorageAccount") ?? String.Empty;
-                    try
-                    {
-                        var blobServiceClient = new BlobServiceClient(storageConnectionString);
+			var data = await reader.ReadNextSectionAsync();
 
-                        var cloudBlobContainer = blobServiceClient.GetBlobContainerClient("bigbeercontainer");
-                        await cloudBlobContainer.CreateIfNotExistsAsync();
+			while (data != null)
+			{
+				var file = data.AsFileSection();
+				using (MemoryStream ms = new())
+				{
+					string storageConnectionString = System.Environment.GetEnvironmentVariable("BigBeerStorageAccount") ?? String.Empty;
+					try
+					{
+						var blobServiceClient = new BlobServiceClient(storageConnectionString);
 
-                        BlockBlobClient cloudBlockBlob = cloudBlobContainer.GetBlockBlobClient(file.FileName);
+						var cloudBlobContainer = blobServiceClient.GetBlobContainerClient("bigbeercontainer");
+						await cloudBlobContainer.CreateIfNotExistsAsync();
 
-                        await file.FileStream.CopyToAsync(ms);
-                        ms.Position = 0;
+						BlockBlobClient cloudBlockBlob = cloudBlobContainer.GetBlockBlobClient(file.FileName);
 
-                        await cloudBlockBlob.UploadAsync(ms);
+						await file.FileStream.CopyToAsync(ms);
+						ms.Position = 0;
 
-                        var props = 
+						await cloudBlockBlob.UploadAsync(ms);
 
-                        await cloudBlockBlob.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = data.ContentType});
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex.Message);
-                        var errorresponse = req.CreateResponse(HttpStatusCode.InternalServerError);
-                        errorresponse.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-                        errorresponse.WriteString(ex.Message);
-                        return errorresponse;
-                    }
-                }
-                data = await reader.ReadNextSectionAsync();
-            }
+						await this.GenerateNotification(ms, cloudBlockBlob, file.FileName);
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+						var props = await cloudBlockBlob.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = data.ContentType });
+					}
+					catch (Exception ex)
+					{
+						_logger.LogError(ex.Message);
+						var errorresponse = req.CreateResponse(HttpStatusCode.InternalServerError);
+						errorresponse.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+						errorresponse.WriteString(ex.Message);
+						return errorresponse;
+					}
+				}
+				data = await reader.ReadNextSectionAsync();
+			}
 
-            response.WriteString("Uploaded to Storage Blob");
+			var response = req.CreateResponse(HttpStatusCode.OK);
+			response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            return response;
-        }
-    }
+			response.WriteString("Uploaded to Storage Blob");
+			return response;
+		}
+
+		protected async Task GenerateNotification(MemoryStream ms, BlockBlobClient blob, string name)
+		{
+			ms.Position = 0;
+			byte[] md5 = MD5.HashData(ms.ToArray());
+			//string sMD5 = Encoding.UTF8.GetString(md5);
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < md5.Length; i++)
+			{
+				sb.Append(md5[i].ToString("x2"));
+			}
+			string sMD5 = sb.ToString();
+
+			try
+			{
+
+				var metadata = new Dictionary<string, string>
+										{
+												{ MD5Key, sMD5 ?? String.Empty }
+										};
+				await blob.SetMetadataAsync(metadata);
+
+				NotificationHubClient clientHub = NotificationHubClient
+						.CreateClientFromConnectionString("Endpoint=sb://BigBeerHub.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=05Q8T0gXQ2QxcP25woaXMtbEqAQP8NOGolQO0+FMUlU=", "TappAppUpdates");
+
+				XmlDocument beerToast = new();
+				beerToast.Load("./datastore/BeerUpdate.xml");
+
+				string tap = name.Substring(0, name.IndexOf('.'));
+
+				XmlNode? textNode = beerToast.SelectSingleNode("/toast/visual/binding/text");
+				if (textNode != null)
+				{
+					textNode.InnerText = $"A beer has been updated on tap #{tap}";
+				}
+				XmlNode? imageNode = beerToast.SelectSingleNode("/toast/visual/binding/image");
+				if (imageNode != null)
+				{
+					((XmlElement)imageNode).SetAttribute("src", $"https://cs1c08048ede1dax4ddbx836.blob.core.windows.net/bigbeercontainer/{tap}.png?m={DateTime.Now.ToBinary()}");
+				}
+				var notificationResult = await clientHub.SendWindowsNativeNotificationAsync(beerToast.InnerXml);
+
+
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, "Error checking upload");
+			}
+
+		}
+	}
 }
