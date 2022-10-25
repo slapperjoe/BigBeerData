8843cb142bcc6b6160b5157275a7929f5d5ed954
slapperjoe <markr@live.com>
2022-10-26 05:00:43 +1100

    Work on price saving

--- Client/DTOs/BeerDTO.cs
+++ Client/DTOs/BeerDTO.cs
@@ -5,7 +5,22 @@
 		public string name { get; set; } = default!;
 		public string brewer { get; set; } = default!;
 		public string description { get; set; } = default!;
-        public decimal schooner { get; set; } = default!;
-        public decimal growler { get; set; } = default!;
-    }
+		public decimal schooner { get; set; } = default!;
+		public decimal squealer { get; set; } = default!;
+		public decimal growler { get; set; } = default!;
+
+		public bool changed { get; set; } = true;
+
+		public string visStyle {  
+			get
+			{
+				if (changed)
+				{
+					return "display: none";
+
+				} 
+				return "";
+			} 
+		}
+	}
 }
