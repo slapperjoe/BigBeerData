cdd70502e73b89a856a47260a1f21c77c18c7e02
slapperjoe <markr@live.com>
2022-11-05 13:39:42 +1100

    async start?

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,27 +9,28 @@ using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
 
 IConfiguration config = new ConfigurationBuilder()
-    .AddEnvironmentVariables()
-    .Build();
+	 .AddEnvironmentVariables()
+	 .Build();
 
 var host = new HostBuilder()
 		.ConfigureFunctionsWorkerDefaults()
-        .ConfigureServices(builder => {
-            builder.AddDbContext<BigBeerContext>(opt =>
-                                    opt.UseSqlServer(
-                                        config["DBConnection"]
-                                    ));
-            builder.AddHttpClient("BeerBot", config =>
-             {
-                 var productValue = new ProductInfoHeaderValue("BBD", "1.0");
-                 var commentValue = new ProductInfoHeaderValue("(Hi)");
+		  .ConfigureServices(builder =>
+		  {
+			  builder.AddDbContext<BigBeerContext>(opt =>
+											  opt.UseSqlServer(
+													config["DBConnection"]
+											  ));
+			  builder.AddHttpClient("BeerBot", config =>
+				{
+					var productValue = new ProductInfoHeaderValue("BBD", "1.0");
+					var commentValue = new ProductInfoHeaderValue("(Hi)");
 
-                 config.BaseAddress = new Uri("https://api.untappd.com/v4/");
+					config.BaseAddress = new Uri("https://api.untappd.com/v4/");
 
-                 config.DefaultRequestHeaders.UserAgent.Add(productValue);
-                 config.DefaultRequestHeaders.UserAgent.Add(commentValue);
-             });
-        })
+					config.DefaultRequestHeaders.UserAgent.Add(productValue);
+					config.DefaultRequestHeaders.UserAgent.Add(commentValue);
+				});
+		  })
 		.Build();
 
-host.Run();
+await host.RunAsync();
