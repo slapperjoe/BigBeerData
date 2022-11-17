973cfd0d35b12bc49396a4a91139e1e7ee7e6847
slapperjoe <markr@live.com>
2022-11-17 15:11:21 +1000

    service worker tag for build actions

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -1,3 +1,5 @@
+const CACHE_VERSION = '{#CACHE_VERSION#}'
+
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
